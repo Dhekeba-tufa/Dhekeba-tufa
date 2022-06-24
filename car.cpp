556991792585a7815934bd8c5c7f3477fa9980a7
Shalom <39420239+Dhekeba-tufa@users.noreply.github.com>
2022-06-24 18:19:07 +0300

    Create car.cpp

---
+++ car.cpp
@@ -0,0 +1,51 @@
+#include<glut.h>
+#include<iostream>//for strlen
+#include<stdlib.h>
+
+ 
+int i,q;
+int score = 0;//for score counting
+int screen = 0;
+bool collide = false;//check if car collide to make game over
+char buffer[10];
+
+int vehicleX = 200, vehicleY = 70;   
+int ovehicleX[4], ovehicleY[4];
+int divx = 250, divy = 4, movd;
+
+ 
+void drawText(char ch[],int xpos, int ypos)//draw the text for score and game over
+{
+    int numofchar = strlen(ch);
+    glLoadIdentity ();
+    glRasterPos2f( xpos , ypos);
+    for (i = 0; i <= numofchar - 1; i++)
+    {
+     
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ch[i]);//font used here, may use other font also
+    }
+}
+ 
+ 
+void drawTextNum(char ch[],int numtext,int xpos, int ypos)//counting the score
+{
+    int len;
+    int k;
+    k = 0;
+    len = numtext - strlen (ch);
+    glLoadIdentity ();
+    glRasterPos2f( xpos , ypos);
+    for (i = 0; i <=numtext - 1; i++)
+    {
+    if ( i < len )
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'0');
+    else
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ch[k]);
+        k++;
+    }
+     
+    }
+}
+ 
+ 
