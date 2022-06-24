e3cc478f26cfc01c6f3231917a98e4fd6a0a9525
Shalom <39420239+Dhekeba-tufa@users.noreply.github.com>
2022-06-24 12:04:05 -0700

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,51 +1,208 @@
-#include<glut.h>
-#include<iostream>//for strlen
-#include<stdlib.h>
+#include<windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
- 
-int i,q;
-int score = 0;//for score counting
-int screen = 0;
-bool collide = false;//check if car collide to make game over
-char buffer[10];
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
 
-int vehicleX = 200, vehicleY = 70;   
-int ovehicleX[4], ovehicleY[4];
-int divx = 250, divy = 4, movd;
+//Game Speed
+int FPS = 50;
+//Game Track
+int start=0;
+int gv=0;
+int level = 0;
 
- 
-void drawText(char ch[],int xpos, int ypos)//draw the text for score and game over
-{
-    int numofchar = strlen(ch);
-    glLoadIdentity ();
-    glRasterPos2f( xpos , ypos);
-    for (i = 0; i <= numofchar - 1; i++)
-    {
-     
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ch[i]);//font used here, may use other font also
+//Track Score
+int score = 0;
+
+//Form move track
+int roadDivTopMost = 0;
+int roadDivTop = 0;
+int roadDivMdl = 0;
+int roadDivBtm = 0;
+
+//For Card Left / RIGHT
+int lrIndex = 0 ;
+
+//Car Coming
+int car1 = 0;
+int lrIndex1=0;
+int car2 = +35;
+int lrIndex2=0;
+int car3 = +70;
+int lrIndex3=0;
+
+//For Display TEXT
+const int font1=(int)GLUT_BITMAP_TIMES_ROMAN_24;
+const int font2=(int)GLUT_BITMAP_HELVETICA_18 ;
+const int font3=(int)GLUT_BITMAP_8_BY_13;
+
+char s[30];
+void renderBitmapString(float x, float y, void *font,const char *string){
+    const char *c;
+    glRasterPos2f(x, y);
+    for (c=string; *c != '\0'; c++) {
+        glutBitmapCharacter(font, *c);
     }
 }
- 
- 
-void drawTextNum(char ch[],int numtext,int xpos, int ypos)//counting the score
-{
-    int len;
-    int k;
-    k = 0;
-    len = numtext - strlen (ch);
-    glLoadIdentity ();
-    glRasterPos2f( xpos , ypos);
-    for (i = 0; i <=numtext - 1; i++)
-    {
-    if ( i < len )
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'0');
-    else
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ch[k]);
-        k++;
-    }
-     
-    }
+
+void tree(int x, int y){
+    int newx=x;
+    int newy=y;
+    //Tree Left
+            //Bottom
+        glColor3f(0.871, 0.722, 0.529);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(newx+11,newy+55);
+        glVertex2f(newx+12,newy+55-10);
+        glVertex2f(newx+10,newy+55-10);
+        glEnd();
+            //Up
+      glColor3f(0.133, 0.545, 0.133);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(newx+11,newy+55+3);
+        glVertex2f(newx+12+3,newy+55-3);
+        glVertex2f(newx+10-3,newy+55-3);
+        glEnd();
 }
- 
- 
+
+
+void startGame(){
+    //Road
+    glColor3f(0.412, 0.412, 0.412);
+    glBegin(GL_POLYGON);
+    glVertex2f(20,0);
+    glVertex2f(20,100);
+    glVertex2f(80,100);
+    glVertex2f(80,0);
+    glEnd();
+
+    //Road Left Border
+    glColor3f(1.000, 1.000, 1.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(20,0);
+    glVertex2f(20,100);
+    glVertex2f(23,100);
+    glVertex2f(23,0);
+    glEnd();
+
+    //Road Right Border
+    glColor3f(1.000, 1.000, 1.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(77,0);
+    glVertex2f(77,100);
+    glVertex2f(80,100);
+    glVertex2f(80,0);
+    glEnd();
+
+    //Road Middel Border
+      //TOP
+    glColor3f(1.000, 1.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(48,roadDivTop+80);
+    glVertex2f(48,roadDivTop+100);
+    glVertex2f(52,roadDivTop+100);
+    glVertex2f(52,roadDivTop+80);
+    glEnd();
+    roadDivTop--;
+    if(roadDivTop<-100){
+        roadDivTop =20;
+        score++;
+    }
+        //Midle
+    glColor3f(1.000, 1.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(48,roadDivMdl+40);
+    glVertex2f(48,roadDivMdl+60);
+    glVertex2f(52,roadDivMdl+60);
+    glVertex2f(52,roadDivMdl+40);
+    glEnd();
+
+
+
+    roadDivMdl--;
+    if(roadDivMdl<-60){
+        roadDivMdl =60;
+        score++;
+    }
+        //Bottom
+    glColor3f(1.000, 1.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(48,roadDivBtm+0);
+    glVertex2f(48,roadDivBtm+20);
+    glVertex2f(52,roadDivBtm+20);
+    glVertex2f(52,roadDivBtm+0);
+    glEnd();
+    roadDivBtm--;
+    if(roadDivBtm<-20){
+        roadDivBtm=100;
+        score++;
+    }
+    //Score Board
+     glColor3f(0.000, 0.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(80,97);
+    glVertex2f(100,97);
+    glVertex2f(100,98-8);
+    glVertex2f(80,98-8);
+    glEnd();
+
+    //Print Score
+    char buffer [50];
+    sprintf (buffer, "SCORE: %d", score);
+    glColor3f(0.000, 1.000, 0.000);
+    renderBitmapString(80.5,95,(void *)font3,buffer);
+    //Speed Print
+    char buffer1 [50];
+    sprintf (buffer1, "SPEED:%dKm/h", FPS);
+    glColor3f(0.000, 1.000, 0.000);
+    renderBitmapString(80.5,95-2,(void *)font3,buffer1);
+    //level Print
+    if(score % 50 == 0){
+        int last = score /50;
+        if(last!=level){
+            level = score /50;
+            FPS=FPS+2;
+
+        }
+    }
+    char level_buffer [50];
+    sprintf (level_buffer, "LEVEL: %d", level);
+    glColor3f(0.000, 1.000, 0.000);
+    renderBitmapString(80.5,95-4,(void *)font3,level_buffer);
+
+    //Increse Speed With level
+
+    //MAIN car
+        //Front Tire
+    glColor3f(0.000, 0.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(lrIndex+26-2,5);
+    glVertex2f(lrIndex+26-2,7);
+    glVertex2f(lrIndex+30+2,7);
+    glVertex2f(lrIndex+30+2,5);
+    glEnd();
+        //Back Tire
+    glColor3f(0.000, 0.000, 0.000);
+    glBegin(GL_POLYGON);
+    glVertex2f(lrIndex+26-2,1);
+    glVertex2f(lrIndex+26-2,3);
+    glVertex2f(lrIndex+30+2,3);
+    glVertex2f(lrIndex+30+2,1);
+    glEnd();
+        //Car Body
+    glColor3f(0.678, 1.000, 0.184);
+    glBegin(GL_POLYGON);
+    glVertex2f(lrIndex+26,1);
+    glVertex2f(lrIndex+26,8);
+    glColor3f(0.000, 0.545, 0.545);
+
+    glVertex2f(lrIndex+28,10);
+    glVertex2f(lrIndex+30,8);
+    glVertex2f(lrIndex+30,1);
+    glEnd();
